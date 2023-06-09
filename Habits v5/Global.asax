1baf7f1714edfc455d3814caf3bd91d7d6b4c3a3
Thato <t.chitja369@gmail.com>
2023-06-09 12:36:12 +0200

    Initial Application Setup

---
+++ Habits v5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Habits_v5.MvcApplication" Language="C#" %>
---
+++ Habits v5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Habits_v5.Startup))]
+namespace Habits_v5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
