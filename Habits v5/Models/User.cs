08386dd669adac614e5f011a0227b463e713a41f
Thato <t.chitja369@gmail.com>
2023-06-09 14:28:41 +0200

    Added Model for User

---
+++ Habits v5/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Habits_v5.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [EmailAddress(ErrorMessage = "The email is invalid")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required")]
+        [MinLength(6, ErrorMessage = "The password requires 6 characters or more")]
+        public string Password { get; set; }
+    }
+}
